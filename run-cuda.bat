ad8a72fd472b0033f678f8ccecb13b984b7db330
OSTADJ2F <amiralitarbiat@gmail.com>
2024-11-15 22:16:48 -0400

    cuda run

--- run-cuda.bat
+++ run-cuda.bat
@@ -1,1 +1,4 @@
-python run.py --execution-provider cuda --execution-threads 60 --max-memory 60
+@echo off
+call .\myenv\scripts\activate
+python run.py --execution-provider cuda --execution-threads 20 --max-memory 30
+pause
